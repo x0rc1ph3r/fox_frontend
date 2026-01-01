446cfdcaf4e103c8a468371018fad8ab1ac18090
Farman Shaik <shkfrmn786@gmail.com>
2026-01-01 10:50:56 +0530

    add nft collection

--- src/utils/verifiedNftCollections.ts
+++ src/utils/verifiedNftCollections.ts
@@ -1,8 +1,8 @@
 export const VerifiedNftCollections = [
     {
-        name: "Test NFT collection",
-        address: "3zNrzedWE3d89Ae24e6jZ88YgQXRnxZ3Sn6cFHMn2dXF",
-        image:"https://imgs.search.brave.com/MiIpQ7QaJdvpiwnTq2AjmIazY3Wrm0UpNyYsFw2hw2o/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9tZWRp/YS5pc3RvY2twaG90/by5jb20vaWQvMTM2/MzgwNDU5NC9waG90/by9uZnQuanBnP3M9/NjEyeDYxMiZ3PTAm/az0yMCZjPWVLS196/MXl2d1BvejhXM2xm/Rmh2RWFvYmpXYWh1/Qmo2ck5qb3pVX3Rf/WFU9",
+        name: "Fox NFT collection",
+        address: "C1jzvtJJQvLuAhwxQShPrirMScivV7g5GXhbbuCz8HAb",
+        image:"https://devnet.irys.xyz/Dc2fQ2itZggWzefzDJNVDrHcuEbY9Wq2fkGGRE7BfYLX",
         twitter:"https://x.com/sakshambhugra",
         website:"#",
     }
