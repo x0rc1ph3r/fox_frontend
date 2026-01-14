e211b1d0d81d4d09d3dcbb70f7ebb0349019ec13
Farman Shaik <shkfrmn786@gmail.com>
2026-01-14 18:12:36 +0530

    add domain

--- src/constants.ts
+++ src/constants.ts
@@ -2,5 +2,5 @@ export const HELIUS_KEY = import.meta.env.VITE_HELIUS_API_KEY;
 // change to "mainnet" when deploying to mainnet
 export const NETWORK = "mainnet" as "devnet" | "mainnet";
 export const SOLANA_RPC = (NETWORK === "devnet") ? "https://api.devnet.solana.com" : `https://mainnet.helius-rpc.com/?api-key=${HELIUS_KEY}`;
-export const API_URL = "";
+export const API_URL = "https://raffle.meccain.com";
 // export const API_URL = "http://3.39.230.132";
